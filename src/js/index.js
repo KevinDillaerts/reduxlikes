7db701a60deef1415d6d7dbd62f6dbe900d79aec
KevinDillaerts <69395404+KevinDillaerts@users.noreply.github.com>
2021-11-03 20:34:13 +0100

    scroll to top added

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import Musicsection from "./components/Musicsection";
 import Photossection from "./components/Photossection";
 import Likezone from "./components/Likezone";
 
+//main variables & set-up
 const main = document.querySelector(".content");
 const likes = document.querySelector(".likezone");
 const openLikes = document.querySelector(".likes__open");
@@ -14,9 +15,33 @@ new Musicsection(main);
 new Photossection(main);
 new Likezone(likes);
 
+//mobile menu buttons
 openLikes.onclick = () => {
   likes.classList.add("active");
 };
 closeLikes.onclick = () => {
   likes.classList.remove("active");
 };
+
+//scroll to top icon
+const scrollIcon = document.querySelector(".scroll");
+scrollIcon.onclick = () =>
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+
+function toggleScroll(entries) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      console.log(entry);
+      scrollIcon.classList.remove("active");
+    } else {
+      scrollIcon.classList.add("active");
+    }
+  });
+}
+
+let observer = new IntersectionObserver(toggleScroll);
+observer.observe(document.querySelector("h1"));
